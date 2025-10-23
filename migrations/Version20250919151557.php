dc51060a7a620e8a00dece20b525b54bd15901a2
Matteo <matteo318p@gmail.com>
2025-10-23 15:54:40 +0200

    nginx+mariadb

--- migrations/Version20250919151557.php
+++ migrations/Version20250919151557.php
@@ -20,7 +20,7 @@ final class Version20250919151557 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE prodotto (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(128) NOT NULL, description CLOB DEFAULT NULL, size INTEGER NOT NULL)');
+        $this->addSql('CREATE TABLE prodotto (id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, name VARCHAR(128) NOT NULL, description TEXT DEFAULT NULL, size INTEGER NOT NULL)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4');
     }
 
     public function down(Schema $schema): void
